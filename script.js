59287e5a788cfbdf974cd804235cc4d6fc2ae256
akcods <ak20ash@gmail.com>
2023-03-10 11:27:08 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// bubble sorting
+/* let arr = [8,2,4,1,3,45,67,89,3,3,12,34,5,6,7,898,23,10,11,100.006];
+let len = arr.length;
+let res=[];
+let isSorted;
+for(let i=0; i<len-1; i++){
+    isSorted=true;
+    for(let j=0; j<len-i; j++){
+        if(arr[j]>arr[j+1]){
+            let temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+            isSorted=false;
+        }
+    }
+    if(isSorted){
+        break;
+    }
+}
+console.log(arr); */
+
+/* let arr = [8,2,4,1,3,45,67,89,3,3,12,34,5,6,7,898,23,10,11,100.006];
+const min = Math.min(...arr);
+console.log(min); */
